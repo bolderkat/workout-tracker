fbaad2fb243e24a41ee6a808b807b35fc49caeef
Daniel Luo <d.luo117@gmail.com>
2025-08-27 08:16:57 -0600

    Initial Commit

---
+++ WWDC21WorkoutApp Watch App/WWDC21WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC21WorkoutAppApp.swift
+//  WWDC21WorkoutApp Watch App
+//
+//  Created by Daniel Luo on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WWDC21WorkoutApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WWDC21WorkoutApp Watch AppTests/WWDC21WorkoutApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC21WorkoutApp_Watch_AppTests.swift
+//  WWDC21WorkoutApp Watch AppTests
+//
+//  Created by Daniel Luo on 8/27/25.
+//
+
+import Testing
+@testable import WWDC21WorkoutApp_Watch_App
+
+struct WWDC21WorkoutApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
